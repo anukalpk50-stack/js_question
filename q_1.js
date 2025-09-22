1dd67f2090a802ff67084b35b8c269f37590604d
anukalpk50-stack <anukalpk50@gmail.com>
2025-09-22 19:48:02 +0530

    new commit

--- q_1.js
+++ q_1.js
@@ -3,9 +3,6 @@
 // arr1 = [1, 2, 3, 4, 5, 6, 7, 8];
 // let arr1 = [1, 2, 3, 4, 5, 6, 7, 8];
 // let arr2 = [2, 34, 4, 56, 6, 6, 6, 8];
-
-const { useReducer } = require("react");
-
 // for ( let i = 2; i < arr1.length; i +=1){
 // let temp =arr1[i];
 // arr1[i] = arr2[i];
@@ -29,3 +26,15 @@ const { useReducer } = require("react");
 // let arr = [20, 22, 50, 53, 60, 80, 98, 90, 78]
 // const divisible_by_10 = arr.filter(num => num % 10 === 0);
 // console.log(divisible_by_10);
+// todo create an array of numbers and take input from the user to add numbers to this array.
+
+
+// todo create an array of square of given numbers.
+
+// todo use reduce to calculate factorial of a given numbers from an array of first n natural numbers. (n being the number where factorial needs to be calculated)
+let arr = [5, 6, 7, 9, 11]
+const factorial = arr.reduce((num1, num2) => {
+    return num1 * num2
+})
+
+console.log(factorial);
